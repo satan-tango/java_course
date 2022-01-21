ac50db35dd0da4a6b3634c059e551c7b4a3544b5
Dmitry Varkovich <d.varkov123darg@mail.ru>
2022-01-21 15:28:23 +0300

    Revert "123123"
    This reverts commit 03decf3959b8ccbeee7bffa8b6c26a5f8009364e.

--- src/com/javacourse/lessons/les_1/homework/Calculator/Calculator.java
+++ src/com/javacourse/lessons/les_1/homework/Calculator/Calculator.java
@@ -7,6 +7,10 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter two numbers : ");
+        System.out.println("Enter two numbers : ");
+        System.out.println("Enter two numbers : ");
+        System.out.println("Enter two numbers : ");
         boolean flagException  = true;
         String firstNumber = "";
         String secondNumber = "";
