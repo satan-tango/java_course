c0c4e066f58f97880a4520709eca0a3ff9e44cf2
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-22 21:02:16 +0300

    Update Task3.java

--- src/com/javacourse/lessons/les_2/homework/task3/Task3.java
+++ src/com/javacourse/lessons/les_2/homework/task3/Task3.java
@@ -1,11 +1,13 @@
 package com.javacourse.lessons.les_2.homework.task3;
 
 //Перевод из дюймов в сантиметры.
-
+//+'
 public class Task3 {
     public static void main(String[] args) {
         System.out.println("inches\tcentimetres");
+        //20 - magic number, use const
         for (int i = 1; i <=20 ; i++) {
+            //2.54 - magic number, use const
             System.out.println(i+"\t\t"+i*2.54);
         }
     }
