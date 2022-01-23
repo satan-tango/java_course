63f10ff3b46a2483de5ed574bb9ebd74ca1e6ab7
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-23 21:28:04 +0300

    Update Task2.java

--- src/com/javacourse/lessons/les_3/homework/task2/Task2.java
+++ src/com/javacourse/lessons/les_3/homework/task2/Task2.java
@@ -1,13 +1,14 @@
 package com.javacourse.lessons.les_3.homework.task2;
 
 //Вывести четные чила от 2 до 100 включительно
-
+//Also I wanted to see from you continue or break usage (doing task not in only 1 option)
 public class Task2 {
     public static void main(String[] args) {
         final int LAST_NUMBER = 100;
         System.out.println("Even numbers:");
         for (int i = 2; i <= LAST_NUMBER; i++) {
             if (i % 2 == 0) {
+                //what does +i mean?
                 System.out.println(+i);
             }
         }
