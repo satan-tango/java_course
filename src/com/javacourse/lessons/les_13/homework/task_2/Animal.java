2b8d04a3b6fec6fc475561cd57d62b168059c873
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-03-19 20:24:30 +0300

    Update Animal.java

--- src/com/javacourse/lessons/les_13/homework/task_2/Animal.java
+++ src/com/javacourse/lessons/les_13/homework/task_2/Animal.java
@@ -1,5 +1,5 @@
 package com.javacourse.lessons.les_13.homework.task_2;
-
+//hm, I don't remember interface discussion! Actually for this task it's bad idea.
 public interface Animal {
     void animalSound();
 }
