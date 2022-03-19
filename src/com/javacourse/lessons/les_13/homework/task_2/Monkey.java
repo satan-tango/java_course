ccff3a2c3d7f6f05452edb5263d0e4ba9dfd5f0b
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-03-19 20:26:11 +0300

    Update Monkey.java

--- src/com/javacourse/lessons/les_13/homework/task_2/Monkey.java
+++ src/com/javacourse/lessons/les_13/homework/task_2/Monkey.java
@@ -1,5 +1,5 @@
 package com.javacourse.lessons.les_13.homework.task_2;
-
+//All your classes have 'private String name'. Use inheritance! Use OOP! It helps you to avoid duplication! 
 public class Monkey implements Animal{
     private String name;
 
