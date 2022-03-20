0af2801befa746863ee911ad4b3a9420ee019728
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-03-20 21:21:46 +0300

    Update CountryHouse.java

--- src/com/javacourse/lessons/les_14/homework/task_1/CountryHouse.java
+++ src/com/javacourse/lessons/les_14/homework/task_1/CountryHouse.java
@@ -1,5 +1,5 @@
 package com.javacourse.lessons.les_14.homework.task_1;
-
+//+
 public class CountryHouse extends Building{
     public CountryHouse(int amountOfFloor, boolean heating, int amountOfResidents) {
         super(amountOfFloor, heating, amountOfResidents);
