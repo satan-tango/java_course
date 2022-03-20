a9ef47b6e484ea44154ec1d3ffbaaf292259b784
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-03-20 21:21:56 +0300

    Update HighRiseBuilding.java

--- src/com/javacourse/lessons/les_14/homework/task_1/HighRiseBuilding.java
+++ src/com/javacourse/lessons/les_14/homework/task_1/HighRiseBuilding.java
@@ -1,5 +1,5 @@
 package com.javacourse.lessons.les_14.homework.task_1;
-
+//+
 public class HighRiseBuilding extends Building{
     public HighRiseBuilding(int amountOfFloor, boolean heating, int amountOfResidents) {
         super(amountOfFloor, heating, amountOfResidents);
