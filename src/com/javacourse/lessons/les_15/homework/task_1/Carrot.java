16e5d4552b5c54555c80234682bc454cfb09da76
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-03-26 15:29:53 +0300

    Update Carrot.java

--- src/com/javacourse/lessons/les_15/homework/task_1/Carrot.java
+++ src/com/javacourse/lessons/les_15/homework/task_1/Carrot.java
@@ -1,5 +1,5 @@
 package com.javacourse.lessons.les_15.homework.task_1;
-
+//no sence in this task as you have TypeOfVegetable in Vegetable
 public class Carrot extends Vegetable{
 
     public Carrot(double weight, TypeOfVegetable typeOfVegetable) {
