ce811d387b79f9a19a7a7a1b242088d32959d35e
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-03-26 15:28:52 +0300

    Update Vegetable.java

--- src/com/javacourse/lessons/les_15/homework/task_1/Vegetable.java
+++ src/com/javacourse/lessons/les_15/homework/task_1/Vegetable.java
@@ -3,6 +3,8 @@ package com.javacourse.lessons.les_15.homework.task_1;
 public abstract class Vegetable {
 
     private double weight;
+    //If you use enum TypeOfVegetable then all of the specific Vegetable instances are redundant, actually... As they do not
+    //have any logic behind
     private TypeOfVegetable typeOfVegetable;
 
     public Vegetable(double weight, TypeOfVegetable typeOfVegetable) {
@@ -12,6 +14,7 @@ public abstract class Vegetable {
 
     public int CalculateTheCalorie() {
         double result = 0;
+        //5->const, 9->const, 4->const
         result = typeOfVegetable.getProteins() * 5 + typeOfVegetable.getFats() * 9 + typeOfVegetable.getCarbohydrates() * 4;
         return (int) (result * weight / 100);
     }
